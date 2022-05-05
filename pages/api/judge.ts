b032e144c449a6865008378c05c6c7a85d7ba7f6
ethan <benidiktusvio@gmail.com>
2022-05-05 23:16:47 +0700

    Update judge.ts

--- pages/api/judge.ts
+++ pages/api/judge.ts
@@ -2,6 +2,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  req.headers["remote-address"] = req.socket.remoteAddress;
+  req.headers["remote-port"] = req.socket.remotePort?.toString();
+
   let headers: object = Object.fromEntries(Object.entries(req.headers).sort());
+
   res.status(200).json(headers);
 }
