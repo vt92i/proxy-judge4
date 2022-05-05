01b5f6053e7aa732ca38f1cd69be79b32f06a3f5
ethan <benidiktusvio@gmail.com>
2022-05-06 00:10:21 +0700

    Add and configure CORS

--- pages/api/judge.ts
+++ pages/api/judge.ts
@@ -1,7 +1,29 @@
+import Cors from 'cors';
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+const cors = Cors({
+  methods: ['GET'],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function initMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Function) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await initMiddleware(req, res, cors);
+
   req.headers["remote-address"] = req.socket.remoteAddress;
   req.headers["remote-port"] = req.socket.remotePort?.toString();
 
@@ -9,3 +31,5 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
   res.status(200).json(headers);
 }
+
+export default handler;
